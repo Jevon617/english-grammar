cfbc6f4240b60682506a4495bba0a7210dfad17c
Mr-ccyou <1787176370@qq.com>
2023-08-29 14:00:31 +0800

    docs: base change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  outDir: 'docs'
+  outDir: 'docs',
+  base: '/docs'
 })
